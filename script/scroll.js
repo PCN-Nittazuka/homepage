a8d97d171b9bc51bfe41473a4e67f69fe3dd07e1
nzPC-E7240 <pcn.nittazuka@gmail.com>
2021-05-24 09:42:28 +0900

    update the homepage by yuito.

--- script/scroll.js
+++ script/scroll.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
         }
     });
     pagetop.click(function () {
-        $('body, html').animate({ scrollTop: 0 }, 10);
+        $('body, html').animate({ scrollTop: 0 }, 0);
         return false;
     });
 });
